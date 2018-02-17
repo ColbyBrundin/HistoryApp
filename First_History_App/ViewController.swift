686cd255f24c497cce9aad429e130180e40c58e1
Colby Brundin <colbybrundin@Colbys-MacBook-Air.local>
2018-02-17 15:14:19 -0500

    Initial Commit

---
+++ First_History_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First_History_App
+//
+//  Created by Colby Brundin on 2/17/18.
+//  Copyright © 2018 Colby Brundin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
